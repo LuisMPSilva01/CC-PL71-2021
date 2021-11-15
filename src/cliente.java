5a97f26030db1950655bc6f0058372fbf239c22c
LuisMPSilva01 <lisinho2001@gmail.com>
2021-11-15 09:13:18 +0000

    First commit

---
+++ src/cliente.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class cliente {
+
+    public static void main(String[] args) throws IOException {
+        if(args.length!=1){
+            System.out.println("Format: FCliente ficheiro");
+            return;
+        }
+        Logger logger = Logger.getLogger("MyLog");
+        FileHandler fh = null;
+
+        try {
+            try {
+                fh = new FileHandler("C:\\Users\\Acer\\Desktop\\logs.txt");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            assert fh != null;
+            fh.setFormatter(formatter);
+
+            // the following statement is used to log any messages
+            logger.info("FCliente " + args[0]);
+
+        } catch(SecurityException e) {
+            e.printStackTrace();
+        }
+
+        Socket socket = null;
+        String host = "127.0.0.1";
+
+        socket = new Socket(host, 8888);
+
+
+        File file = new File(args[0]);
+        // Get the size of the file
+        long length = file.length();
+        byte[] bytes = new byte[16 * 1024];
+        InputStream in = new FileInputStream(file);
+        OutputStream out = socket.getOutputStream();
+
+        int count;
+        while ((count = in.read(bytes)) > 0) {
+            out.write(bytes, 0, count);
+        }
+
+        out.close();
+        in.close();
+        socket.close();
+    }
+}
